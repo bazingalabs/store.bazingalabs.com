cccaa8137fd082039449566988750b172af2ba2a
Erik Kallen <info@erikkallen.nl>
2012-07-25 22:50:20 +0200

    automatic deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,50 +1,60 @@
-require 'bundler/capistrano'
-server "store.bazingalabs.com", :web, :app, :db, primary: true
+require "bundler/capistrano"
+load 'deploy/assets'
 
-set :application, "store.bazingalabs.com"
-set :user, "deployer"
-set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote_cache
-set :use_sudo, false
+set :application, "spree"
+set :user, 'spree'
+set :group, 'www-data'
+set :rails_env, 'production'
 
-set :scm, "git"
-set :repository, "git@github.com:bazingalabs/store.bazingalabs.com.git"
-set :branch, "master"
+role :web, '78.47.33.92'
+role :app, '78.47.33.92'
+role :db,  '78.47.33.92', :primary => true
+
+set :scm, :git
+set :repository,  "git://github.com/bazingalabs/store.bazingalabs.com.git"
+set :branch,      "master"
+set :deploy_to,   "/data/#{application}"
+set :deploy_via,  :remote_cache
+set :use_sudo,    false
 
 default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+set :ssh_options, { :forward_agent => true }
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export, :roles => :app do
+    run "cd #{current_path} && bundle exec foreman export upstart /etc/init"
+  end
+
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "start #{application}"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "stop #{application}"
+  end
+
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    sudo "restart #{application}"
+  end
+end
 
 namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
-  end
-
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
-  end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :symlink_config, roles: :app do
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/Procfile #{release_path}/Procfile"
+    run "ln -nfs #{shared_path}/config/.foreman #{release_path}/.foreman"
   end
-  after "deploy:finalize_update", "deploy:symlink_config"
-
-  desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
-    end
-  end
-  before "deploy", "deploy:check_revision"
 end
+
+before 'deploy:assets:precompile', 'deploy:symlink_shared'
+
+before 'deploy:start', 'foreman:export'
+after 'deploy:start', 'foreman:start'
+
+before 'deploy:restart', 'foreman:export'
+after 'deploy:restart', 'foreman:restart'
